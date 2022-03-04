2f5ec48c9317148fdc6756e63c43b04b5e5f83a4
Júlio Guilherme <julioguilhermedossantos@yahoo.com.br>
2022-03-04 12:15:51 -0300

    Alterando o retorno do método authenticate ResponseEntity<String> -> ResponseEntity<User>

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,10 +1,11 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Login;
+import com.example.demo.model.User;
 import com.example.demo.service.LoginService;
 import org.springframework.http.ResponseEntity;
 
 public interface LoginController {
     LoginService getLoginService();
-    ResponseEntity<String> authenticate(Login login);
+    ResponseEntity<User> authenticate(Login login);
 }
