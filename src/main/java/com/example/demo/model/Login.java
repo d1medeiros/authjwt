6b73ec37adbd7eea25eeef44f4db2b70e785f317
Júlio Guilherme <julioguilhermedossantos@yahoo.com.br>
2022-03-04 17:39:27 -0300

    Javax validation

--- src/main/java/com/example/demo/model/Login.java
+++ src/main/java/com/example/demo/model/Login.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Login {
+
+    @NotEmpty
     private String username;
+    @NotEmpty
     private char[] password;
 }
