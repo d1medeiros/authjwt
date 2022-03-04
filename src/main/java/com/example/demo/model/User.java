08dd6f976a90d34a88d790869d11b18d0eeaa5a0
Rodrigo Vidal <rodrigo.vidal@b2wdigital.com>
2022-03-04 12:03:49 -0300

    Criado método para inserir as datas no update e create Password não pode ser nulo e não serializado pelo jackson

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,8 +18,21 @@ public class User {
     private Long id;
     @Column(unique = true, nullable = false)
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false)
     private String password;
     private LocalDateTime dateCreate;
     private LocalDateTime lastUpdate;
     private boolean active;
+
+    @PrePersist
+    public void prePersist() {
+        dateCreate = LocalDateTime.now();
+        lastUpdate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        lastUpdate = LocalDateTime.now();
+    }
 }
